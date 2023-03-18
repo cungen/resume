9f9842a7025d4812d1b8c2f1285c8449ffc8c1d1
cungen <1360571582@qq.com>
2023-03-18 18:56:18 +0800

    fix: sm width issue in info wrapper

--- src/components/InfoWrapper.vue
+++ src/components/InfoWrapper.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="flex flex-col sm:flex-row print:flex-row">
             <div
-                class="w-28 flex flex-none justify-start pr-3 text-xl text-gray-500 leading-7 mb-3 sm:w-28 sm:justify-end sm:text-base print:justify-end print:text-base"
+                class="flex flex-none justify-start pr-3 text-xl text-gray-500 leading-7 mb-3 sm:w-28 sm:justify-end sm:text-base print:justify-end print:text-base"
             >
                 <slot name="left"></slot>
             </div>
